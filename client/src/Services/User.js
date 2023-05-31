53eeb037cccf6cf0df65242c89497ccc419c4a3e
Dhiraj455 <dhirajshelke16@gmail.com>
2023-06-01 01:01:58 +0530

    Url updated

--- client/src/Services/User.js
+++ client/src/Services/User.js
@@ -1,20 +1,22 @@
 import axios from "axios";
 // const axios = require("axios").default;
 
+let url = "https://dhiraj-dobby1.onrender.com"
+
 export const register = async (form) => {
-  return await axios.post(`/register`, form, {
+  return await axios.post(`${url}/register`, form, {
     headers: { "Content-Type": "application/json" },
   });
 };
 
 export const login = async (form) => {
-  return await axios.post(`/login`, form, {
+  return await axios.post(`${url}/login`, form, {
     headers: { "Content-Type": "application/json" },
   });
 };
 
 export const Logout = async () => {
-  return await axios.get(`/logout`, {
+  return await axios.get(`${url}/logout`, {
     headers: { "Content-Type": "application/json" },
   });
 };
