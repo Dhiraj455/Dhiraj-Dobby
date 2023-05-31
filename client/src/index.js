56fca515c49ca215798b4d5a68a5c598f0ccb1c9
Dhiraj455 <dhirajshelke16@gmail.com>
2023-06-01 01:16:09 +0530

    Updated Routes

--- client/src/index.js
+++ client/src/index.js
@@ -1,15 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import { BrowserRouter } from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
 import "remixicon/fonts/remixicon.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import './index.css';
+import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  // <BrowserRouter>
+  <App />
+  // </BrowserRouter>
 );
-
