5a0b42073ef6d8c119960127e5f3f1943269bc53
Dhiraj455 <dhirajshelke16@gmail.com>
2023-06-01 01:17:52 +0530

    Updated

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { BrowserRouter } from "react-router-dom";
+// import { BrowserRouter } from "react-router-dom";
 import "remixicon/fonts/remixicon.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.css";
