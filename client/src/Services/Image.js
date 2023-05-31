f47b35b64cfd3eed5274a880220625450286aae1
Dhiraj455 <dhirajshelke16@gmail.com>
2023-06-01 00:58:20 +0530

    Url updated

--- client/src/Services/Image.js
+++ client/src/Services/Image.js
@@ -4,13 +4,13 @@ export const uploadImage = async (form) => {
   // return await axios.post(`/postImage`, form, {
   //   headers: { "Content-Type": "multipart/form-data" },
   // });
-  return await axios.post(`/postImage`, form, {
+  return await axios.post(`https://dhiraj-dobby1.onrender.com/postImage`, form, {
     header: { "Content-Type": "multipart/form-data" },
   });
 };
 
 export const getUserImages = async (search) => {
-  return await axios.get(`/getUserImages?search=${search}`, {
+  return await axios.get(`https://dhiraj-dobby1.onrender.com/getUserImages?search=${search}`, {
     headers: { "Content-Type": "application/json" },
   });
 }
